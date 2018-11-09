847674be240e0bc043b6891e4da0ea53d0c0ecc0
Gabriel Tan <tangabrielkl@gmail.com>
2018-11-09 10:24:51 -0500

    Added OnError event to websocket class. Implemented try-catch block in websocket class

--- QuestradeAPI/QuestradeWebsocket.cs
+++ QuestradeAPI/QuestradeWebsocket.cs
@@ -15,43 +15,78 @@ namespace QuestradeAPI.Websocket
 
         public event EventHandler<MessageEventArg> OnReceive;
 
+        public event EventHandler<ErrorEventArg> OnError;
 
         public async Task ConnectAsync(Uri uri, System.Threading.CancellationToken cancel)
         {
-            await ws.ConnectAsync(uri, cancel);
-
-            if(ws.State == WebSocketState.Open)
+            try
             {
-                MessageEventArg arg = new MessageEventArg();
-                arg.message = "Connection Established";
-                OnConnect(this, arg);
-                RecieveAsync(cancel);
+                if (ws.State == WebSocketState.Open)
+                {
+                    MessageEventArg arg = new MessageEventArg();
+                    arg.message = "Connection Established";
+                    OnConnect(this, arg);
+                    RecieveAsync(cancel);
+                }
             }
+            catch(WebSocketException ex)
+            {
+                ErrorEventArg arg = new ErrorEventArg();
+                arg.time = DateTime.Now;
+                arg.socketState = ws.State;
+                arg.socketException = ex;
+                OnError(this, arg);
+            }
+            await ws.ConnectAsync(uri, cancel);
+            
         }
 
         private async void RecieveAsync(System.Threading.CancellationToken cancel)
         {
-            ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[10000]);
-            WebSocketReceiveResult result = await ws.ReceiveAsync(buffer, cancel);
-            MessageEventArg arg = new MessageEventArg();
-            if (result.Count != 0 || result.CloseStatus == WebSocketCloseStatus.Empty)
+            try
             {
-                arg.time = DateTime.Now;
-                string message = Encoding.ASCII.GetString(buffer.Array,
-                     buffer.Offset, result.Count);
-                arg.message = message;
-                OnReceive(this, arg);
-                RecieveAsync(cancel);
+                ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[10000]);
+                WebSocketReceiveResult result = await ws.ReceiveAsync(buffer, cancel);
+                MessageEventArg arg = new MessageEventArg();
+                if (result.Count != 0 || result.CloseStatus == WebSocketCloseStatus.Empty)
+                {
+                    arg.time = DateTime.Now;
+                    string message = Encoding.ASCII.GetString(buffer.Array,
+                         buffer.Offset, result.Count);
+                    arg.message = message;
+                    OnReceive(this, arg);
+                    RecieveAsync(cancel);
+                }
             }
+            catch(WebSocketException ex)
+            {
+                ErrorEventArg arg = new ErrorEventArg();
+                arg.time = DateTime.Now;
+                arg.socketState = ws.State;
+                arg.socketException = ex;
+                OnError(this, arg);
+            }
+            
         }
 
         public async Task SendAsync(string message, Uri uri, System.Threading.CancellationToken cancel, System.Text.Encoding encoding)
         {
-            ArraySegment<byte> buffer = new ArraySegment<byte>( encoding.GetBytes(message.ToCharArray()));
-            
-            await ws.SendAsync(buffer, WebSocketMessageType.Text, true, cancel);
+            try
+            {
+                ArraySegment<byte> buffer = new ArraySegment<byte>(encoding.GetBytes(message.ToCharArray()));
 
-            System.Diagnostics.Debug.WriteLine("Sent success.");
+                await ws.SendAsync(buffer, WebSocketMessageType.Text, true, cancel);
+
+                System.Diagnostics.Debug.WriteLine("Sent success.");
+            }
+            catch (WebSocketException ex)
+            {
+                ErrorEventArg arg = new ErrorEventArg();
+                arg.time = DateTime.Now;
+                arg.socketState = ws.State;
+                arg.socketException = ex;
+                OnError(this, arg);
+            }
         }
 
 
--- QuestradeAPI/QuestradeWebsocketEvents.cs
+++ QuestradeAPI/QuestradeWebsocketEvents.cs
@@ -8,4 +8,11 @@ namespace QuestradeAPI.Websocket.Events
         public string message;
         public DateTime time;
     }
+
+    public class ErrorEventArg : EventArgs
+    {
+        public DateTime time;
+        public System.Net.WebSockets.WebSocketState socketState;
+        public System.Net.WebSockets.WebSocketException socketException;
+    }
 }
